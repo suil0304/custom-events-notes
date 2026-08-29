76a559e30b1322cd236d5e6b41ed0c711111e511
suil0304 <eagle030410@gmail.com>
2026-08-29 19:56:40 +0900

    refactor: IEventHandlerOwner with EventHandlerOwner

--- source/interfaces/IEventHandlerOwner.hx
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-import funkin.modding.module.Module;
-import interfaces.IEventHandleable;
-
-interface IEventHandlerOwner<T:(Module & IEventHandleable)> {
-    public var handler:T;
-}
