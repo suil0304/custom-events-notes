ec3b240ec5b51e2bb3c331b1b19b73ff9adaec19
suil0304 <eagle030410@gmail.com>
2026-08-29 19:46:58 +0900

    feat: Add CharacterPosition abstract enum

--- source/data/CharacterPosition.hx
+++ source/data/CharacterPosition.hx
@@ -1,7 +1,31 @@
 package data;
 
-enum abstract CharacterPosition(String) to String from String {
-    var BOYFRIEND = "bf";
-    var DADDY = "dad";
-    var GIRLFRIEND = "gf";
+import funkin.play.character.BaseCharacter.CharacterType;
+
+enum abstract CharacterPosition(CharacterType) to CharacterType {
+    var BF = CharacterType.BF;
+    var DAD = CharacterType.DAD;
+    var GF = CharacterType.GF;
+    var OTHER = CharacterType.OTHER;
+
+    @:from
+    static function fromString(v:String):CharacterPosition {
+        return switch(v) {
+            case "bf": BF;
+            case "dad": DAD;
+            case "gf": GF;
+            case "other": OTHER;
+            default: throw 'Invalid character position: $v';
+        }
+    }
+
+    @:to
+    function toString():String {
+        return switch(this) {
+            case CharacterType.BF: "bf";
+            case CharacterType.DAD: "dad";
+            case CharacterType.GF: "gf";
+            case CharacterType.OTHER: "other";
+        };
+    }
 }
